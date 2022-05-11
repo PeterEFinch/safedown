33ccf03fe62f3153c22836ca3db44483a27e8444
pfinch <pfinch.mathphys@gmail.com>
2022-05-12 07:57:14 +1000

    :sparkles: Introducing shutdown actions

--- safedown.go
+++ safedown.go
@@ -1,1 +1,35 @@
 package safedown
+
+import (
+	"sync"
+)
+
+type ShutdownActions struct {
+	actions []func()   // actions contains the functions to be run on shutdown
+	mutex   sync.Mutex // mutex prevents clashes when shared across goroutines
+}
+
+func (sa *ShutdownActions) AddActions(actions ...func()) {
+	sa.mutex.Lock()
+	sa.actions = append(sa.actions, actions...)
+	sa.mutex.Unlock()
+}
+
+func (sa *ShutdownActions) Shutdown() {
+	sa.shutdown()
+}
+
+func (sa *ShutdownActions) shutdown() {
+	/*
+		Shutdown logic was separated into its own method in anticipation that
+		there will be multiple ways of triggering the shutdown actions.
+	*/
+
+	sa.mutex.Lock()
+	actions := sa.actions[:]
+	sa.mutex.Unlock()
+
+	for i := range actions {
+		actions[i]()
+	}
+}
