ed103a85069565a0402f9f7291cca20ad71f9041
Peter Finch <pfinch.mathphys@gmail.com>
2022-05-22 06:58:47 +1000

    ✨ Introducing HTTP server example (#22)

---
+++ examples/http_server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/PeterEFinch/safedown"
+)
+
+func main() {
+	// Initialising shutdown actions. Any actioned added is called if a signal
+	// is received.
+	sa := safedown.NewShutdownActions(
+		safedown.UseOrder(safedown.FirstInLastDone), // This option is unnecessary because it is the default.
+		safedown.UsePostShutdownStrategy(safedown.PerformImmediately),
+		safedown.ShutdownOnAnySignal(),
+		safedown.UseOnSignalFunc(func(signal os.Signal) {
+			log.Printf("Signal received: %s\n", signal.String())
+		}),
+	)
+	defer sa.Shutdown()
+
+	server := &http.Server{
+		Addr: "address",
+		Handler: http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
+			// Do something ...
+		}),
+	}
+	sa.AddActions(func() {
+		// The timeout and error handling should be set by the user.
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("http server failed to shutdown: %v\n", err)
+		}
+	})
+
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		log.Printf("http server encountered error: %v\n", err)
+	}
+}
